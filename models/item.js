6c2daf034114ef102507d1aca757bd70cd95c743
Timothy Miamba <timal0361@gmail.com>
2024-07-05 17:44:29 +0200

    chore: Fix Item schema bugs

--- models/item.js
+++ models/item.js
@@ -3,13 +3,15 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ItemSchema = new Schema({
-  name: { String, required: true },
-  description: { String, required: true },
+  name: { type: String, required: true },
+  description: { type: String, required: true },
   category: { type: Schema.Types.ObjectId, ref: "Category", required: true },
-  price: { Number, required: true },
+  price: { type: Number, required: true },
   stock: Number,
 });
 
 ItemSchema.virtual("url").get(function () {
   return `/shop/item/${this._id}`;
 });
+
+module.exports = mongoose.model("Item", ItemSchema);
