1e65fcecae5aef2e6ab983dc3d5cd148f821e187
Timothy Miamba <timal0361@gmail.com>
2024-07-26 16:56:37 +0200

    feat: Add item creation functionality

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,3 +1,4 @@
+const category = require("../models/category");
 const Category = require("../models/category");
 const Item = require("../models/item");
 const asyncHandler = require("express-async-handler");
@@ -49,3 +50,62 @@ exports.item_create_get = asyncHandler(async (req, res, next) => {
     errors: null,
   });
 });
+
+exports.item_create_post = [
+  // Validate and sanitize fields.
+  body("name", "Name must have at least 3 characters")
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+  body("description", "Description must have at least 3 colours")
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+  body("price", "Price must be greater than 0").isFloat({ gt: 0 }).toFloat(),
+  body("stock", "Number in stock value must be greater than 0")
+    .isFloat({ gt: 0 })
+    .toFloat(),
+
+  // Process request after validation and sanitization.
+  asyncHandler(async (req, res, next) => {
+    // Extract validation errors from the request.
+    const errors = validationResult(req);
+
+    // Create an Item object with escaped and trimmed data.
+    const item = new Item({
+      name: req.body.name,
+      description: req.body.description,
+      category: req.body.category,
+      price: req.body.price,
+      stock: req.body.stock,
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render the form again with sanitized values/error messages.
+      const allCategories = await Category.find().sort({ name: 1 }).exec();
+
+      res.render("item_form", {
+        title: "Create Item",
+        categories: allCategories,
+        item: item,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      // Data from the form is valid.
+      // Check if an item with the same name exists
+      const itemExists = await Item.findOne({ name: req.body.name })
+        .collation({ locale: "en", strength: 2 })
+        .exec();
+
+      if (itemExists) {
+        // Item exists. Redirect to its detail page.
+        res.redirect(itemExists.url);
+      } else {
+        await item.save();
+        // New item saved. Redirect to its detail page.
+        res.redirect(item.url);
+      }
+    }
+  }),
+];
--- routes/shop.js
+++ routes/shop.js
@@ -34,9 +34,7 @@ router.get("/items", item_controller.item_list);
 
 router.get("/item/create", item_controller.item_create_get);
 
-router.post("/item/create", function (req, res) {
-  return res.send("POST request to create item");
-});
+router.post("/item/create", item_controller.item_create_post);
 
 router.get("/item/:id", item_controller.item_detail);
 
